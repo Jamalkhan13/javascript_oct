d108cdec43a727cb36facc56097501836d146081
Jamal khan <149874834+Jamalkhan13@users.noreply.github.com>
2024-10-04 12:15:43 +0500

    container

---
+++ 01_basics/stack_and_heap.js
@@ -0,0 +1,21 @@
+// let myname = "jamal khan";
+// anotherName = myname
+// anotherName = "khan khan"
+
+// console.log(myname);
+// console.log(anotherName);
+
+
+ let userOne = {
+  name : "jamal khan",
+  city : "swat" 
+
+}
+
+let userTwo = userOne
+    userTwo.city="islamabad";
+    console.log(userOne.city);
+    console.log(userTwo.city);
+    
+    
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,90 @@
+// let score = "33abc";
+
+
+// console.log(score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let value = false
+// const valueOf = Number(value)
+// console.log(typeof valueOf);
+// console.log(valueOf);
+
+
+// let num = 1;
+
+// let isLoggedIn = Boolean(num)
+// console.log(isLoggedIn);
+// console.log(typeof isLoggedIn);
+
+
+// let bolvalue = 0;
+// console.log(typeof bolvalue);
+
+// let boolIS = Boolean(bolvalue)
+// console.log(boolIS);
+// console.log(typeof boolIS);
+
+
+// let name = " ";
+// console.log(name);
+
+// let changeToBoolean = Boolean(name);
+// console.log(changeToBoolean);
+// console.log(typeof changeToBoolean);
+
+
+
+ let x = 2;
+  const y = x++;
+ console.log(`"x :" ${x}, "y :" ${y}`);
+
+
+let a = 1;
+ const b = ++a;
+ console.log(`"a :" ${a}, "b :" ${b}`);
+
+
+
+const id = Symbol('123')
+
+const anotherId = Symbol('123')
+
+
+
+console.log(id);
+console.log(anotherId);
+console.log(id === anotherId);
+
+
+
+const score = 100;
+const scoreValue =100.34;
+ const isLoggedIn = false;
+ const outSideTemp = null;
+ let userEmail;
+ const bigNumber = 123456789654567245453454534545345345453545n
+
+ const myFunction = function() {
+  console.log(myFunction);
+  
+ }
+
+ let myObj = {
+        name : "jamal khan",
+        age: 24,
+ }
+
+ const city = ["peshawar","islamabad","lahore"]
+
+ console.table([typeof score, typeof scoreValue,typeof isLoggedIn,typeof outSideTemp, typeof userEmail,typeof myFunction ,typeof myObj, typeof bigNumber, typeof city]);
+ 
+
+
+
+
+
+

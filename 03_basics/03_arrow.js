05e012cbe1287b6973d7e9a39f6bedd34fba280e
Jamal khan <149874834+Jamalkhan13@users.noreply.github.com>
2024-10-10 16:24:19 +0500

    arrow function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,30 @@
+const user = {
+  username : "jamal",
+  price: 999,
+  welcomeMessage:function () {
+    console.log(`${this.username}, and price is ${this.price}`);
+   // console.log(this);
+    
+  }
+}
+
+// user.welcomeMessage()
+// user.username = "sam";
+// user.welcomeMessage()
+
+console.log(this);
+
+// const addTwo=  (num1,num2) =>{
+//     return num1+num2
+// }
+// console.log(addTwo(5,6))
+
+const addTwo = (num1,num2) => num1 + num2
+const addThree = (num1,num2,nym3) => (num1 + num2)
+console.log(addTwo(5,5));
+console.log(addThree(5,5,5));
+
+
+// return object in implist 
+const objValue = ()=> ({name:"Jamal khan"})
+console.log(objValue());

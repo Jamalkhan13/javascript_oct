a51ef5a002b8f07e80a3dde77567dd4733b77275
Jamal khan <149874834+Jamalkhan13@users.noreply.github.com>
2024-10-14 12:05:04 +0500

    switch

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,41 @@
+const month = 3;
+
+switch (month){
+  case 1:
+    console.log("january");
+    break;
+  case 2:
+    console.log("febuary");
+    break;
+  case 3:
+    console.log("march");
+     break;
+  case 4:
+    console.log("april");
+     break;
+    default :
+    console.log("default case match");
+     break;    
+}
+
+const lock = "three"
+
+switch (lock){
+  case "one" :
+    console.log("incorrect password");
+    break;
+  case "two" :
+    console.log("two is incorrect");
+    break;
+  case "three" :
+    console.log("password match opened");
+     break;   
+  case "four" :
+    console.log("four is incorrect");
+    break;
+  default :
+    console.log("error");
+     break;         
+}
+
+

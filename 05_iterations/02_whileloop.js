f8944bc71d8d4947a47f27963441e88feb779531
jamalkhan13 <jamalkhan7858@gmail.com>
2024-10-15 15:02:14 +0500

    while and do while loops

---
+++ 05_iterations/02_whileloop.js
@@ -0,0 +1,33 @@
+let val = 0;
+while (val<=10) {
+  console.log(val);
+   val = val +2;
+}
+
+
+let myArray = ["i phone","samaung","vivo","infinix"]
+
+let arr = 0;
+while(arr<myArray.length){
+  console.log(`${myArray[arr]}`);
+  arr++; 
+}
+
+
+// do while 
+
+let balance = 100;
+do {
+  console.log(`your score is ${balance}`);
+  balance--;
+  
+} while (balance>80);
+
+
+let products = ["watches","laptops","mobiles","power bank"]
+let index = 0;
+do {
+    console.log(` ${products[index]}`);
+    index++;
+    
+} while (index<products.length);

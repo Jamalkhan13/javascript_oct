31554b607a38040c9cdbe4532a97a6c0a3fd85e8
jamalkhan13 <jamalkhan7858@gmail.com>
2024-10-15 10:27:29 +0500

    terniary operator

---
+++ 04_control_flow/05_terniary_operator.js
@@ -0,0 +1,8 @@
+const coursePrice = 500;
+coursePrice <= 700 ? console.log("less than 700") : console.log("more than 700");
+
+
+const val = 30;
+val<30 ? console.log("below 30") : console.log("eqal");
+
+

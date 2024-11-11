548bc5dbde09266d33b27f240dfc9f5010d0aafc
jamalkhan13 <jamalkhan7858@gmail.com>
2024-11-11 22:12:23 +0500

    edit DOM

--- 06_DOM/DOM_04/index.js
+++ 06_DOM/DOM_04/index.js
@@ -15,10 +15,17 @@ function addOptimiseLanguage (languageName) {
   // instead of innerHTML will use createTextNode
   // const addText = document.createTextNode(languageName)
   // li.appendChild(addText)
-  
+
   // we can code direct in one line instead of storing in variable and then append in child
   li.appendChild(document.createTextNode(languageName))
   document.querySelector('.language').appendChild(li)
 }
 addOptimiseLanguage('mySQL')
 addOptimiseLanguage('mongoDB')
+
+// Edit 
+const secondLanguage = document.querySelector("li:nth-child(1)")
+// secondLanguage.innerHTML = "Mojo" // not a good practise 
+const newli = document.createElement('li')
+newli.textContent = "rust"
+secondLanguage.replaceWith(newli)

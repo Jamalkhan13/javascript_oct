b1856cc2c3fdbbc74de34ce19ec44cb6db32ec8b
Jamalkhan13 <jamalkhan7858@gmail.com>
2024-11-23 12:33:24 +0500

    color switcher

--- 07_projects/project_01/project_01/index.js
+++ 07_projects/project_01/project_01/index.js
@@ -1,5 +1,6 @@
 const buttons = document.querySelectorAll('.button')
 const body = document.querySelector("body")
+const textElement = document.querySelectorAll('.text')
 
 buttons.forEach((button)=>{
   console.log(button);
@@ -9,20 +10,41 @@ buttons.forEach((button)=>{
     console.log(e.target);
     if(e.target.id === 'grey'){
       body.style.backgroundColor = "gray" // or e.target.id 
+
+      textElement.forEach((txt)=>{
+        txt.style.color = 'blue'
+      })
     }
     else if(e.target.id === 'yellow'){
       body.style.backgroundColor = e.target.id
       body.style.color = "black"
+
+      textElement.forEach((txt)=>{
+        txt.style.color = 'black'
+      })
     }
     else if(e.target.id === "blue"){
       body.style.backgroundColor = e.target.id
       body.style.color = "white"
+
+      textElement.forEach((txt)=>{
+        txt.style.color = 'yellow'
+        txt.style.backgroundColor = "red"
+      })
     }
     else if(e.target.id === 'white'){
       body.style.backgroundColor = e.target.id
       body.style.color = ""
+
+      textElement.forEach((txt)=>{
+        txt.style.color = 'purple'
+      })
     }else if (e.target.id === "purple"){
       body.style.backgroundColor = e.target.id
+
+      textElement.forEach((txt)=>{
+        txt.style.color = 'orange'
+      })
     }
   })  
 })

b147fa786ee35a111c15c1ecc791c4dfa8d9a2e8
Jamalkhan13 <jamalkhan7858@gmail.com>
2024-12-26 13:29:00 +0500

    Random Color Generator

---
+++ 09_events/Random color generator/index.js
@@ -0,0 +1,40 @@
+const startButon = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+
+
+// generate a random colo
+
+const randomColor = function(){
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for(let i=0; i<6;i++){
+    color += hex[Math.floor(Math.random()*16)]
+  }
+  return color;
+}
+
+console.log(randomColor());
+
+let intervalId;
+const startChangingColor = function(){
+
+  if(!intervalId){
+    intervalId = setInterval(changeBgColor,1000)
+  }
+
+  function changeBgColor(){
+    document.body.style.backgroundColor = randomColor()
+  }
+  
+};
+const stopChangingColor = function (){
+  clearInterval(intervalId)
+  intervalId= null;
+};
+  
+
+
+startButon.addEventListener("click", startChangingColor);
+stopButton.addEventListener("click",stopChangingColor);
+
+

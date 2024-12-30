e0c8bb1752cc951a6947697046f5ad3c864660e5
Jamalkhan13 <jamalkhan7858@gmail.com>
2024-12-30 15:53:33 +0500

    promises

---
+++ 10_advance/promises.js
@@ -0,0 +1,64 @@
+const promiseOne = new Promise(function(resolve,reject){
+  setTimeout(function(){
+    console.log("Async task is complete");
+    resolve()
+    
+  },1000)
+})
+
+promiseOne.then(function(){
+  console.log("promiseone is consumed");
+  
+})
+
+
+new Promise(function(resolve,reject){
+  setTimeout(function(){
+    console.log("Async task 2");
+    resolve();
+  },1000)
+}).then(function(){
+  console.log("then resolved");
+  
+})
+
+
+const promiseThree = new Promise(function(resolve,reject){
+  setTimeout(function(){
+    resolve({userName : "jamalkhan", userEmail: "jamal@yahoo.com"})
+  },1000)
+})
+
+promiseThree.then(function(user){
+  console.log(user.userEmail);
+  
+})
+
+
+const promiseFour = new Promise(function(resolve,reject){
+  setTimeout(function(){
+    let error = true;
+    if(!error){
+      resolve({user:"Jamal", password: "1123"})
+    }
+    else{
+      reject("something went wrong")
+    }
+  },1000)
+})
+
+promiseFour.then(function(user){
+  console.log(user);
+  return user.password
+})
+.then((data)=>{
+  console.log(data);
+  
+})
+.catch(function(error){
+  console.log(error);
+  
+}).finally(()=>{
+  console.log("The promise is either resolved or rejected");
+  
+})
